93fea2f4ae5f0c0afcb110a791f63dfead055a8b
desantisb <desantisb@gmail.com>
2021-05-17 17:27:39 +0200

    Update keymap.c

--- keyboards/aluplanck/keymaps/default/keymap.c
+++ keyboards/aluplanck/keymaps/default/keymap.c
@@ -38,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_planck_grid(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-        NUMPAD, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
+        KC_LCTL, KC_LALT, KC_LGUI, NUMPAD,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [_LOWER] = LAYOUT_planck_grid(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
@@ -54,15 +54,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_BRMD, KC_BRMU, KC_MPLY
     ),
     [_ADJUST] = LAYOUT_planck_grid(
-        _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-        _______, OUT_USB, OUT_BT,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______,  _______,  _______,  _______,  _______,
-        _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
+        _______,  RESET,   _______,   _______, _______, _______, _______, _______, _______,  _______, _______, KC_DEL ,
+        _______,  OUT_USB, OUT_BT,  _______,   _______,  _______, _______, _______,  _______, _______, _______, _______,
+        _______,  _______,  _______,  _______,   _______,  _______,   _______,  _______, _______, _______, _______, _______,
         KC_POWER, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
     ),
     [_NUMPAD] = LAYOUT_planck_grid(
-        _______, _______,   _______,   _______, _______, _______, KC_AGAIN, KC_P7,   KC_P8,    KC_P9, _______, _______ ,
+        _______, _______,   _______,   _______, _______, _______, _______, KC_P7,   KC_P8,    KC_P9, _______, _______ ,
         _______, _______,   _______,   _______, _______, _______, _______, KC_P4,   KC_P5,    KC_P6, _______, _______ ,
-        _______, KC_UNDO,   KC_CUT,    KC_COPY, KC_PASTE, _______, _______, KC_P1,   KC_P2,    KC_P3, _______, _______ ,
+        _______, _______,   _______,   _______, _______, _______, _______, KC_P1,   KC_P2,    KC_P3, _______, _______ ,
         _______, _______,   _______,   _______, _______, _______, _______, _______, KC_P0,  _______, _______, _______
     )
 };
